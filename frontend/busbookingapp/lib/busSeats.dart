47e1224b4e3b59fa52c653c61e44fbb6f36682d3
samson mubeya <rasspartah@gmail.com>
2025-02-07 03:18:01 +0300

    Book seat by selecting seat and view total cost

--- frontend/busbookingapp/lib/busSeats.dart
+++ frontend/busbookingapp/lib/busSeats.dart
@@ -8,46 +8,92 @@ class BusSeatsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Bus Seats Layout'),
-        ),
-        body: BusSeats(),
-      ),
+      debugShowCheckedModeBanner: false,
+      home: BusSeats(),
     );
   }
 }
 
 class BusSeats extends StatefulWidget {
+  const BusSeats({super.key});
+
   @override
   State<BusSeats> createState() => _BusSeatsState();
 }
 
 class _BusSeatsState extends State<BusSeats> {
+  // Custom seat layout (1 = seat, "a" = aisle, 2 = special seat)
+  final List<List<dynamic>> seatLayout = [
+    [1, 1, "a", "a", 2],
+    [1, 1, "a", 1, 1],
+    ["a", "a", "a", 1, 1],
+    [1, 1, "a", 1, 1],
+    [1, 1, "a", 1, 1],
+    [1, 1, "a", 1, 1],
+    [1, 1, "a", 1, 1],
+    [1, 1, 1, 1, 1],
+  ];
+
+  // Mapping seat selection
+  List<List<bool>> selectedSeats = [];
+
+  @override
+  void initState() {
+    super.initState();
+    selectedSeats = List.generate(seatLayout.length,
+        (row) => List.generate(seatLayout[row].length, (col) => false));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(2.0),
-      child: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 4,
-        ),
-        itemCount: 40,
-        itemBuilder: (context, index) {
-          return Container(
-            margin: const EdgeInsets.all(2.0),
-            color: Colors.blue,
-            child: Center(
-              child: Text(
-                (index + 1).toString(),
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 20.0,
-                ),
-              ),
+    return Scaffold(
+      appBar: AppBar(title: Text('Custom Arranged Bus Seats')),
+      body: Column(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Column(
+              children: List.generate(seatLayout.length, (rowIndex) {
+                return Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children:
+                      List.generate(seatLayout[rowIndex].length, (colIndex) {
+                    var seat = seatLayout[rowIndex][colIndex];
+
+                    // Handle aisle space
+                    if (seat == "a") {
+                      return SizedBox(width: 40);
+                    }
+
+                    return GestureDetector(
+                      onTap: () {
+                        setState(() {
+                          selectedSeats[rowIndex][colIndex] =
+                              !selectedSeats[rowIndex][colIndex];
+                        });
+                      },
+                      child: Padding(
+                        padding: EdgeInsets.all(4.0),
+                        child: Icon(
+                          Icons.event_seat,
+                          size: 40,
+                          color: selectedSeats[rowIndex][colIndex]
+                              ? Colors.red // Selected
+                              : (seat == 2
+                                  ? Colors.green
+                                  : Colors.blue), // Normal & Special Seats
+                        ),
+                      ),
+                    );
+                  }),
+                );
+              }),
             ),
-          );
-        },
+          ),
+          Text(
+              'selected seat is ${selectedSeats.expand((row) => row).where((seat) => seat).length}'),
+          // Container()
+        ],
       ),
     );
   }
